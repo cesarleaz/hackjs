900517e53ef1b4a2d88659dec0d2963ac714a5d2
holagabriel.dev <holagabriel.dev@gmail.com>
2024-04-23 13:59:23 +0000

    Update footer's text

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -32,18 +32,14 @@ export default function Footer() {
   return (
     <footer className="rounded-lg m-4 bg-black/90">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <div className="flex items-center text-sm sm:text-center text-white gap-2">
           <a
             href="https://github.com/celelab/"
             className="hover:underline"
             target="_blank"
             rel="noopener noreferrer"
           >
-            © 2024. César Leañez.
+            Creado con 💛 por César Leañez.
           </a>
-          <span classname="flex space-x-0.5">Hecho con mucho 💛 y</span>
-          <JavascriptLogoSVG width="1rem" height="1rem"/>
-        </div>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
           {links.map(({ name, href }) => (
             <FooterLink key={name} name={name} href={href} />
