8b30ab9dbbba4df4499c512611b005c21064f151
holagabriel.dev <holagabriel.dev@gmail.com>
2024-04-23 14:08:18 +0000

    Add call-to-action

---
+++ src/constants/call-to-action.js
@@ -0,0 +1,21 @@
+import CodeSVG from '~/icons/code.svg?react'
+import GithubSVG from '~/icons/github.svg?react'
+import JavaScriptSVG from '~/icons/javascript.svg?react'
+
+export const CALL_TO_ACTION = [
+    {
+        title: 'Guarda tu progreso',
+        description: 'Inicia sesión con Github',
+        icon: GithubSVG,
+    },
+    {
+        title: 'Mejora tus habilidades',
+        description: 'Desafíos interactivos',
+        icon: CodeSVG,
+    },
+    {
+        title: 'En tu lenguaje favorito',
+        description: 'JavaScript',
+        icon: JavaScriptSVG,
+    }
+]
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import Hero from '~/components/hero'
 import ListOfChallenges from '~/components/list-of-challenges'
+import CTABar from '~/components/cta-bar'
 
 export default function HomePage() {
     return (
         <>
             <Hero />
+            <CTABar />
             <ListOfChallenges />
         </>
     )
